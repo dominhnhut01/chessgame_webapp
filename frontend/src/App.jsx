9634d55aa6bcca25ba794147ebaa83560ff4ebea
dominhnhut01 <dominhnhut01@gmail.com>
2023-05-20 03:33:37 -0400

    working frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import ChessBoard from "./components/ChessBoard";
+
+function App() {
+  return (
+    <div className="app">
+      <ChessBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ChessBoard.jsx
@@ -0,0 +1,145 @@
+import { useState, useEffect } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import { socket } from "../socket";
+
+function ChessBoard() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  const [game, setGame] = useState(new Chess());
+  const [moveFrom, setMoveFrom] = useState("");
+  const [rightClickedSquares, setRightClickedSquares] = useState({});
+  const [moveSquares, setMoveSquares] = useState({});
+  const [optionSquares, setOptionSquares] = useState({});
+
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function getMoveOptions(square) {
+    const moves = game.moves({
+      square,
+      verbose: true,
+    });
+    if (moves.length === 0) {
+      return false;
+    }
+
+    const newSquares = {};
+    moves.map((move) => {
+      newSquares[move.to] = {
+        background:
+          game.get(move.to) &&
+          game.get(move.to).color !== game.get(square).color
+            ? "radial-gradient(circle, rgba(0,0,0,.1) 85%, transparent 85%)"
+            : "radial-gradient(circle, rgba(0,0,0,.1) 25%, transparent 25%)",
+        borderRadius: "50%",
+      };
+      return move;
+    });
+    newSquares[square] = {
+      background: "rgba(255, 255, 0, 0.4)",
+    };
+    setOptionSquares(newSquares);
+    return true;
+  }
+
+  function computerMakeMove(playerMoveFrom, playerMoveTo) {
+    const possibleMoves = game.moves();
+
+    // exit if the game is over
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
+      return;
+
+    socket.emit('playerMakeMove', playerMoveFrom, playerMoveTo, (computerMove) => {
+      safeGameMutate((game) => {
+        console.log(`Computer move: ${computerMove}`)
+        game.move(computerMove);
+      });
+    })
+  }
+
+  function onSquareClick(square) {
+    setRightClickedSquares({});
+
+    function resetFirstMove(square) {
+      const hasOptions = getMoveOptions(square);
+      if (hasOptions) setMoveFrom(square);
+    }
+
+    // from square
+    if (!moveFrom) {
+      resetFirstMove(square);
+      return;
+    }
+
+    // attempt to make move
+    const gameCopy = { ...game };
+    const move = gameCopy.move({
+      from: moveFrom,
+      to: square,
+      promotion: "q", // always promote to a queen for example simplicity
+    });
+    setGame(gameCopy);
+
+    // if invalid, setMoveFrom and getMoveOptions
+    if (move === null) {
+      resetFirstMove(square);
+      return;
+    }
+
+    setTimeout(computerMakeMove, 10000, moveFrom, square);
+    setMoveFrom("");
+    setOptionSquares({});
+  }
+
+  function onSquareRightClick(square) {
+    const colour = "rgba(0, 0, 255, 0.4)";
+    setRightClickedSquares({
+      ...rightClickedSquares,
+      [square]:
+        rightClickedSquares[square] &&
+        rightClickedSquares[square].backgroundColor === colour
+          ? undefined
+          : { backgroundColor: colour },
+    });
+  }
+
+  useEffect(() => {
+    socket.emit("handshake", async () => {
+      setIsConnected(true);
+    });
+  }, []);
+
+  if (!isConnected) {
+    return <div className="app">Loading...</div>;
+  }
+
+  return (
+    <div className="baordWrapper">
+      <Chessboard
+        id="ClickToMove"
+        animationDuration={200}
+        arePiecesDraggable={false}
+        position={game.fen()}
+        onSquareClick={onSquareClick}
+        onSquareRightClick={onSquareRightClick}
+        customBoardStyle={{
+          borderRadius: "4px",
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
+        }}
+        customSquareStyles={{
+          ...moveSquares,
+          ...optionSquares,
+          ...rightClickedSquares,
+        }}
+        boardWidth={900}
+      />
+    </div>
+  );
+}
+export default ChessBoard;
