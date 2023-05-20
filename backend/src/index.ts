5fbb1d9551b28eb8d69e6055e698ec512b50dd46
dominhnhut01 <dominhnhut01@gmail.com>
2023-05-20 03:33:06 -0400

    working but slow AI version

---
+++ backend/src/index.ts
@@ -0,0 +1,61 @@
+import http from 'http';
+import express from 'express';
+import { ServerSocket } from './socket';
+
+const app = express();
+
+/** Server Handling */
+const httpServer = http.createServer(app);
+
+/** Start Socket */
+new ServerSocket(httpServer);
+
+/** Log the request */
+app.use((req, res, next) => {
+    console.info(`METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        console.info(`METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
+    });
+
+    next();
+});
+
+/** Parse the body of the request */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/** Rules of our API */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+/** Healthcheck */
+app.get('/ping', (req, res, next) => {
+    return res.status(200).json({ hello: 'world!' });
+});
+
+/** Socket Information */
+// app.get('/status', (req, res, next) => {
+//     return res.status(200).json({ users: ServerSocket.instance.rooms });
+// });
+
+/** Error handling */
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+/** Listen */
+httpServer.listen(5000, () => console.info(`Server is running`));
---
+++ backend/src/socket.ts
@@ -0,0 +1,59 @@
+import { Server as HttpServer } from "http";
+import { Socket, Server } from "socket.io";
+import { v4 } from "uuid";
+import ChessAIEngine from "./model/model";
+import { Chess } from "chess.js";
+
+export class ServerSocket {
+  public static instance: ServerSocket;
+  public io: Server;
+  private aiEngine: ChessAIEngine;
+
+  /** Master list of all connected rooms */
+
+  constructor(server: HttpServer) {
+    
+    ServerSocket.instance = this;
+    this.io = new Server(server, {
+      serveClient: false,
+      pingInterval: 10000,
+      pingTimeout: 5000,
+      cookie: false,
+      cors: {
+        origin: "*",
+      },
+    });
+
+    this.io.on("connect", this.StartListeners);
+  }
+
+  StartListeners = (socket: Socket) => {
+    console.info("Message received from " + socket.id);
+    this.aiEngine = new ChessAIEngine();
+
+    socket.on("handshake", (callback: () => void) => {
+      console.info("Handshake received from: " + socket.id);
+
+      console.info("Sending callback ...");
+      callback();
+    });
+
+    socket.on(
+      "playerMakeMove",
+      (
+        playerMoveFrom: string,
+        playerMoveTo: string,
+        computerMakeMove: (computerMove: string) => void
+      ) => {
+        this.aiEngine.updatePlayerMove(playerMoveFrom, playerMoveTo);
+        const computerMove: string = this.aiEngine.computerMakingMove();
+        console.log(`Computer making move: ${computerMove}`);
+        computerMakeMove(computerMove);
+      }
+    );
+
+    socket.on("disconnect", () => {
+      console.info("Disconnect received from: " + socket.id);
+    });
+  };
+}
